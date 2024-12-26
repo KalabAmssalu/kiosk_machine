7f389a78caeba58877b6545ff012dec42fd3d6ea
Betelhemmesele <betimesele93@gmail.com>
2024-12-26 21:04:59 +0300

    landing page

--- src/app/[locale]/dashboard/home/page.tsx
+++ src/app/[locale]/dashboard/home/page.tsx
@@ -1,11 +1,12 @@
 import KioskMemberForm from "@/components/module/LedgerForm/KioskForm";
+import { LandingPage } from "@/components/screen/home/LandingPage";
 
 type Props = {};
 
 const page = (props: Props) => {
 	return (
 		<div>
-			<KioskMemberForm />
+			<LandingPage />
 		</div>
 	);
 };
